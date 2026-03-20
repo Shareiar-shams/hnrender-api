225e1167313265750e89461f65c242a6143fdbce
Shareiar <islamshareiar@gmail.com>
2026-03-20 07:00:20 +0600

    build(db): switch to PostgreSQL adapter for Prisma client

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,4 +1,7 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma'
 
-const prisma = new PrismaClient();
-export default prisma;
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const prisma = new PrismaClient({ adapter })
+
+export default prisma
